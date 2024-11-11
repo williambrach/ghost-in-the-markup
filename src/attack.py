3e9d3a9c3e4a4ed4753e073c16319b8de0a3f8bc
Matej Petrík <petrix12@gmail.com>
2024-11-11 17:40:13 +0100

    added opfuscation attack

--- src/attack.py
+++ src/attack.py
@@ -1,8 +1,11 @@
 import copy
+import random
+import string
 
 from bs4 import BeautifulSoup
 
 
+# attack using prompt
 def prompt_attack(original_html_bfs: BeautifulSoup) -> BeautifulSoup:
 
     # create deep copy
@@ -37,6 +40,50 @@ def prompt_attack(original_html_bfs: BeautifulSoup) -> BeautifulSoup:
 
     # inject payload
     modified_html.body.insert(0, payload_div)
+
+    # return modified
+    return modified_html
+
+
+# attack by opfuscation
+def opfuscation(original_html_bfs: BeautifulSoup) -> BeautifulSoup:
+
+    # create deep copy
+    modified_html = copy.deepcopy(original_html_bfs)
+
+    # get all divs
+    divs = modified_html.find_all("div")
+
+    # iterate over divs to modify text
+    for div in divs:
+
+        text = div.get_text()
+
+        wrapped_text = ""
+
+        for _i, char in enumerate(text):
+            rand_symbol = random.choices(string.ascii_letters + string.punctuation)
+            wrapped_text += f'{char}<div class="opf">{rand_symbol}</div>'
+
+        div.string = wrapped_text
+
+    # class to hide payload
+    style_tag = modified_html.head.find("style")
+    if not style_tag:
+        style_tag = modified_html.new_tag("style")
+        modified_html.head.append(style_tag)
+
+    # class of payload
+    playload_class = """
+        .opf {
+            display: none;
+        }
+        """
+
+    # add class
+    style_tag.string = (
+        style_tag.string + playload_class if style_tag.string else playload_class
+    )
 
     # return modified
     return modified_html
